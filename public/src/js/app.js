3f3324f3ec3c418241fa638426d3d29536ea59f8
Michał Maciorowski <mmaciorowski@MacBook.local>
2020-05-12 22:00:19 +0200

    service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,14 @@
+var defferedPrompt;
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function() {
         console.log('service worker registered!');
     });
 }
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    defferedPrompt = event;
+    return false;
+});
